5cb1477c1418b2aa08e627f8594ac703ded6e687
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-05-20 18:48:04 +0200

    Function Timer in Benchmark.js

--- assets/JavaScript/benchmarck.js
+++ assets/JavaScript/benchmarck.js
@@ -1,1 +1,14 @@
+/* funzione per il timer. il timer si attiva all'avvio della pagina. per modificarne la durata cambiare il valore della variabile sec */
+let timer;
+let ele = document.getElementById("timer");
 
+(function timerFunction() {
+  let sec = 40;
+  timer = setInterval(() => {
+    ele.innerHTML = sec; //
+    sec--;
+    if (sec < 0) {
+      clearInterval(timer);
+    }
+  }, 1000);
+})();
