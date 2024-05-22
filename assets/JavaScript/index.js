825989964d0744e37f29fdcb72bffe2c5e27d389
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-22 16:52:13 +0200

    fixed proceed button

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -2,14 +2,14 @@ const form = document.getElementById("form");
 const checkbox = document.getElementById("checkbox");
 const proceedButton = document.getElementById("proceedButton");
 
-proceedButton.addEventListener("click", function (params) {});
 proceedButton.addEventListener("click", function (event) {
+  event.preventDefault();
   console.log("Tentativo di invio del modulo"); // Log del tentativo di invio del modulo
   if (!checkbox.checked) {
-    event.preventDefault(); // Impedisce l'azione del link
     console.log("Checkbox non selezionata"); // Log della mancata selezione della checkbox
     alert("Devi accettare i termini per procedere.");
   } else {
+    location.href = "benchmark.html";
     console.log("Checkbox selezionata"); // Log della selezione della checkbox
   }
 });
