c1b26e66fcbc73a268983863e2a2722751460a50
Cecchy2 <dariocecchinato@gmail.com>
2024-05-22 17:08:21 +0200

    corretto cambio messaggio in caso di risposte sbagliate

--- assets/JavaScript/results.js
+++ assets/JavaScript/results.js
@@ -5,13 +5,18 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
   },
   {
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
   },
@@ -27,7 +32,8 @@ const questions = [
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -35,7 +41,8 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question: "What is the most preferred image format used for logos in the Wikimedia database?",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
   },
@@ -45,7 +52,11 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
   },
   {
     category: "Science: Computers",
@@ -110,15 +121,21 @@ wrongAnswersPercentage.innerText = percentualeRisposteSbagliateNumero + "%";
 const wrongAnswersPercentageDonught = document.getElementById("wrongPercentage");
 const correctAnswersPercentageDonught = document.getElementById("correctPercentage");
 
-wrongAnswersPercentageDonught.setAttribute("stroke-dasharray", `${percentualeRisposteSbagliateNumero} ${remainingPercentageWrongNumero}`);
-correctAnswersPercentageDonught.setAttribute("stroke-dasharray", `${percentualeRisposteEsatteNumero} ${remainingPercentageCorrectNumero}`);
+wrongAnswersPercentageDonught.setAttribute(
+  "stroke-dasharray",
+  `${percentualeRisposteSbagliateNumero} ${remainingPercentageWrongNumero}`
+);
+correctAnswersPercentageDonught.setAttribute(
+  "stroke-dasharray",
+  `${percentualeRisposteEsatteNumero} ${remainingPercentageCorrectNumero}`
+);
 
 console.log(percentualeRisposteSbagliateNumero, remainingPercentageWrongNumero);
 console.log(percentualeRisposteEsatteNumero, remainingPercentageCorrectNumero);
 
 if (risposteSbagliate > 5) {
   const congrat = document.getElementById("congrat");
-  congrat.value = "";
+  congrat.value = " ";
   const h3 = document.getElementById("h3");
   const paragrafo = document.getElementById("paragrafo");
   h3.innerText = "MI DISPIACE";
