03bd2bc41ff55be9227d3b1453392b57742a57e2
Andrè pipitone <pipitone37@gmail.com>
2024-05-22 12:50:03 +0200

    Update index.js

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -248,14 +248,20 @@ resultCorrectAnswers.innerText = correctAnswers;
 
 const form = document.getElementById("form");
 const checkbox = document.getElementById("checkbox");
+const proceedButton=document.getElementById("proceedButton")
 
-form.addEventListener("submit", function (event) {
-  console.log("modulo inviato");
+proceedButton.addEventListener("click", function(params) {
+  
+})
+proceedButton.addEventListener('click', function(event) {
+  console.log('Tentativo di invio del modulo'); // Log del tentativo di invio del modulo
   if (!checkbox.checked) {
-    event.preventDefault();
-    console.log("checkbox non selezionata");
-    alert("Devi accettare i termini e le condizioni per continuare.");
+      event.preventDefault(); // Impedisce l'azione del link
+      console.log('Checkbox non selezionata'); // Log della mancata selezione della checkbox
+      alert('Devi accettare i termini per procedere.');
   } else {
-    console.log("checkbox selezionata");
+      console.log('Checkbox selezionata'); // Log della selezione della checkbox
+   
   }
 });
+
