9f30fa5a420fc48453e82e3555cfab0155efc653
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-22 10:31:43 +0200

    Update index.js

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -220,20 +220,16 @@ totalQuestionNumber.forEach((currentElement) => {
 const resultCorrectAnswers = document.getElementById("resultCorrectAnswersNumber");
 resultCorrectAnswers.innerText = correctAnswers;
 
-
-
-const form = document.getElementById ("form");
+const form = document.getElementById("form");
 const checkbox = document.getElementById("checkbox");
 
-form.addEventListener("submit", function(event) {
-  console.log("modulo inviato")
-  if (!checkbox.checked){
+form.addEventListener("submit", function (event) {
+  console.log("modulo inviato");
+  if (!checkbox.checked) {
     event.preventDefault();
     console.log("checkbox non selezionata");
     alert("Devi accettare i termini e le condizioni per continuare.");
-  } else{
+  } else {
     console.log("checkbox selezionata");
   }
-  
 });
-
