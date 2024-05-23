e8bf5704991ef7240990834d00fde0fbb0817fd7
A. Alberto Coscia <angeloalberto.coscia@gmail.com>
2024-05-23 11:14:01 +0200

    adds dynamic selection of benchmark difficulty

--- assets/JavaScript/index.js
+++ assets/JavaScript/index.js
@@ -5,11 +5,20 @@ const proceedButton = document.getElementById("proceedButton");
 proceedButton.addEventListener("click", function (event) {
   event.preventDefault();
   console.log("Tentativo di invio del modulo"); // Log del tentativo di invio del modulo
+
+  const difficulty = document.getElementById("diffic").value;
+
   if (!checkbox.checked) {
     console.log("Checkbox non selezionata"); // Log della mancata selezione della checkbox
     alert("Devi accettare i termini per procedere.");
   } else {
-    location.href = "benchmark.html";
+    if (difficulty === "easy") {
+      window.location.href = "benchmarkEasy.html";
+    } else if (difficulty === "medium") {
+      window.location.href = "benchmarkMedium.html";
+    } else if (difficulty === "hard") {
+      window.location.href = "benchmarkHard.html";
+    }
     console.log("Checkbox selezionata"); // Log della selezione della checkbox
   }
 });
