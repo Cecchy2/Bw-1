db4b94ffbdf774ac8bb1e884bc4df29e4c4b6355
Cecchy2 <dariocecchinato@gmail.com>
2024-05-24 09:36:02 +0200

    cambiati titoli da h1 a h3

--- assets/JavaScript/ringraziamenti.js
+++ assets/JavaScript/ringraziamenti.js
@@ -6,7 +6,7 @@ if (voto <= 6 && voto > 4) {
   const votoAlto = document.getElementById("votoAlto");
   votoAlto.classList.add("invisibile");
   const divVotoMedio = document.getElementById("votoMedio");
-  const testoMedio = document.createElement("h1");
+  const testoMedio = document.createElement("h3");
   testoMedio.innerText =
     "Grazie per aver condiviso la tua opinione con noi! Apprezziamo il tuo feedback di " +
     voto +
@@ -16,8 +16,10 @@ if (voto <= 6 && voto > 4) {
   const votoAlto = document.getElementById("votoAlto");
   votoAlto.classList.add("invisibile");
   const divVotoBasso = document.getElementById("votoBasso");
-  const testoBasso = document.createElement("h1");
+  const testoBasso = document.createElement("h3");
   testoBasso.innerText =
-    "Grazie per aver condiviso il tuo feedback. Siamo dispiaciuti di non aver soddisfatto le tue aspettative e apprezziamo il tuo tempo nel farci sapere come possiamo migliorare. Il tuo contributo è importante per noi e lavoreremo per offrirti un'esperienza migliore in futuro.";
+    "Grazie per aver condiviso il tuo feedback. Siamo dispiaciuti di non aver soddisfatto le tue aspettative e aver preso solamente " +
+    voto +
+    " stelle. Apprezziamo il tuo tempo nel farci sapere come possiamo migliorare. Il tuo contributo è importante per noi e lavoreremo per offrirti un'esperienza migliore in futuro.";
   divVotoBasso.appendChild(testoBasso);
 }
